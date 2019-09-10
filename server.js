7422f4eaa99d8cd975ebc7440f27481e286d590f
HB <hbianan@gmail.com>
2019-09-09 21:44:04 -0400

    file updated
    - updated the server.js
    - added more dependencies

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
-const express = require("express");
-
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// -------------------------- dependencies for this app -------------------------- //
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const LocalStrategy   = require('passport-local');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
 
-// Define middleware here
+// -------------------------- Configure Passport/Possport-Local -------------------------- //
+
+// requires the model with Passport-Local Mongoose plugged in
+const User = require('./models/user');
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// -------------------------- Define middleware here -------------------------- //
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tripfund");
+// a special area of the session used for storing messages
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Start the API server
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tripfund');
+
+// Start the API serverd  
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+
