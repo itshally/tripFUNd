4a3c57af3dde9e7e0648b66024edbb26ec3349cf
HB <hbianan@gmail.com>
2019-09-09 21:11:46 -0400

    files added
    - created user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+//dependencies for this file
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+//creating a collection for users
+const UserSchema = new Schema({
+    username : {
+         type : String,
+         unique : true,
+         required : true
+    },
+    password : {
+         type : String,
+         required : true
+    }
+});
+
+//plugin the passport-local-mongoose into the UserSchema
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", UserSchema);
+
+//exporting User model
+module.exports = User;
