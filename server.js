dd43981c866df0b875856e0b5a42da4e1e8aab86
HB <hbianan@gmail.com>
2019-09-09 22:55:01 -0400

    testing
    - testing done
    - deployment in localhost is now successful

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-// -------------------------- dependencies for this app -------------------------- //
+// -------------------------- Dependencies for this app -------------------------- //
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const LocalStrategy   = require('passport-local');
 const passport = require('passport');
@@ -8,10 +7,29 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const session = require('express-session');
 const routes = require('./routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// -------------------------- Define middleware here -------------------------- //
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/*'));
+}
+
+// add routes, both API and view
+// app.use(routes);
+
+// a special area of the session used for storing messages
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // -------------------------- Configure Passport/Possport-Local -------------------------- //
-
 // requires the model with Passport-Local Mongoose plugged in
 const User = require('./models/user');
 
@@ -22,28 +40,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// -------------------------- Define middleware here -------------------------- //
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tripfund');
 
-// Add routes, both API and view
-app.use(routes);
-
-// a special area of the session used for storing messages
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tripfund');
-
-// Start the API serverd  
+// start the API serverd  
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
