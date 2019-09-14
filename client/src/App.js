a933ab26fde2051363656476bcd36c9a35684800
HB <hbianan@gmail.com>
2019-09-14 16:54:02 -0400

    files updated
    - added files for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './styles/foundation.min.css';
+import './styles/custom.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from './routes';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+
+constructor(){
+super();
+this.state={
+appName: "tripFund",
+home: false
+}
 }
 
+
+render() {
+return (
+<div className="off-canvas-wrapper">
+<div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+<div className="off-canvas-content" data-off-canvas-content>
+<Header name={this.state.appName}/>
+<Routes name={this.state.appName}/>
+<Footer/>
+</div>
+</div>
+</div>
+);
+}
+}
 export default App;
