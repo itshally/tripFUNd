c828dd517c9358103825856dd9d0645863509b9c
HB <hbianan@gmail.com>
2019-09-16 19:32:15 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,12 +3,12 @@ import axios from "axios";
 export default {
   //saves a user to the database
   createUser: user => {
-    return axios.post("api/user", user);
+    return axios.post("/api/user", user);
   },
   findUser: user => {
-    return axios.get("api/user", user);
+    return axios.get("/api/user", user);
   },
   userAuthentication: user => {
-    return axios.post("api/user/login", user);
+    return axios.post("/api/user/login", user);
   }
 };
